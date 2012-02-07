4170609ef77083913fb93bc920b2500201bc1c7f
Andreas Fidjeland <akf@doc.ic.ac.uk>
2012-02-07 13:34:06 +0000

    Test: untargeted synapse type

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -401,6 +401,35 @@ emptySynapseType(backend_t backend, bool haveSynapses)
 
 
 
+/* Test that we produce a warning when synapses have not target, and therefore
+ * will have no effect. This is most likely an error */
+void
+untargetedSynapseType(backend_t backend)
+{
+	nemo::Network net;
+
+	unsigned synapse0 = net.addSynapseType();
+	unsigned synapse1 = net.addSynapseType();
+
+	unsigned iz = net.addNeuronType("Izhikevich", 1, &synapse1);
+
+	float args[7] = {0.02f, 0.25f, -65.0f, 2.0f, 0.0f, -13.0f, -65.0f};
+	for(unsigned nidx=0; nidx < 10; ++nidx) {
+		net.addNeuron(iz, nidx, 7, args);
+	}
+
+	/* Neuron 1 receives input here... */
+	net.addSynapse(synapse1, 0, 1, 1U, 5.0f);
+
+	/* ...but not here */
+	net.addSynapse(synapse0, 0, 1, 1U, 5.0f);
+
+	nemo::Configuration conf = configuration(false, 1024, backend);
+
+	boost::scoped_ptr<nemo::Simulation> sim;
+	/* We should therefore expect an error here */
+	BOOST_REQUIRE_THROW(sim.reset(nemo::simulation(net, conf)), nemo::exception);
+}
 
 
 /* Run a regular ring network test, but with an additional variable-sized
@@ -1100,6 +1129,7 @@ BOOST_AUTO_TEST_SUITE(synapse_types)
 	TEST_ALL_BACKENDS_N(double_ring, runDoubleRing, true)
 	TEST_ALL_BACKENDS_N(empty1, emptySynapseType, false)
 	TEST_ALL_BACKENDS_N(empty2, emptySynapseType, false)
+	TEST_ALL_BACKENDS(untargeted, untargetedSynapseType)
 BOOST_AUTO_TEST_SUITE_END()
 
 
