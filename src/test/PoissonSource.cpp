593c92a9a494406b0562d9ed44eea15ed276c3f5
Andreas Fidjeland <akf@doc.ic.ac.uk>
2012-02-13 11:28:47 +0000

    Test: updated Poisson tests to use new API

--- src/test/PoissonSource.cpp
+++ src/test/PoissonSource.cpp
@@ -6,11 +6,19 @@ namespace nemo {
 void
 testRate(backend_t backend, unsigned duration, bool otherNeurons)
 {
-	nemo::izhikevich::Network net;
+	nemo::Network net;
+	unsigned s_iz = 0U;
+	unsigned n_iz = 0U;
+	if(otherNeurons) {
+		/* Create neuron and synapse type 0 for izhikevich neuron ring */
+		s_iz = net.addSynapseType();
+		n_iz = net.addNeuronType("Izhikevich", 1, &s_iz);
+	}
+
 	nemo::Configuration conf = configuration(false, 1024, backend);
 	if(otherNeurons) {
 		/* This population will never fire */
-		createRing(&net, 0U, 1024, 1);
+		createRing(&net, n_iz, s_iz, 1024, 1);
 	}
 	unsigned poisson = net.addNeuronType("PoissonSource", 0, NULL);
 	float rate = 0.010f;
